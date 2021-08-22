03005ec0abfe17ec7055d6395c283de98b83aa25
rubailly <rubailly@gmail.com>
2021-08-22 21:11:29 +0200

    Configuring display for Gender and Diabetes status options

--- src/test/java/com/savics/WebAppApplicationTests.java
+++ src/test/java/com/savics/WebAppApplicationTests.java
@@ -2,15 +2,14 @@ package com.savics;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.savics.medicalrecords.SavicsMedicalRecordsApplication;
 
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SavicsMedicalRecordsApplication.class)
+@SpringBootTest (classes = SavicsMedicalRecordsApplication.class)
 @WebAppConfiguration
 public class WebAppApplicationTests {
 
