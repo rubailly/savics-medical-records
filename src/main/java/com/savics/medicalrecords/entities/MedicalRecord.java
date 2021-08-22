0980d22428286c48ee333276f588674bc9af58ce
rubailly <rubailly@gmail.com>
2021-08-22 20:12:10 +0200

    Changed from using MySQL to H2 Database

--- src/main/java/com/savics/medicalrecords/entities/MedicalRecord.java
+++ src/main/java/com/savics/medicalrecords/entities/MedicalRecord.java
@@ -1,34 +1,67 @@
 package com.savics.medicalrecords.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Medical Record entity.
  */
 @Entity
+@Table(name = "medical_record")
 public class MedicalRecord {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
-	private String medicalRecordId;
-	private String firtName;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
 	private String lastName;
 
 	public enum Gender {
 		MALE, FEMALE;
 	};
+
+	@Column(name = "gender")
 	private Gender gender;
+
+	@Column(name = "age")
+	private int age;
+
+	@Column(name = "city")
 	private String city;
+
+	@Column(name = "country")
 	private String country;
 
 	public enum LivingWithDiabetes {
 		YES, NO, UNKNOWN;
 	};
+
+	@Column(name = "living_with_diabetes")
 	private LivingWithDiabetes livingWithDiabetes;
+
+	public MedicalRecord() {
+	}
+
+	public MedicalRecord(String firstName, String lastName, Gender gender, int age, String city, String country,
+			LivingWithDiabetes livingWithDiabetes) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.age = age;
+		this.city = city;
+		this.country = country;
+		this.livingWithDiabetes = livingWithDiabetes;
+	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -37,20 +70,12 @@ public class MedicalRecord {
 		this.id = id;
 	}
 
-	public String getMedicalRecordId() {
-		return medicalRecordId;
+	public String getFirstName() {
+		return firstName;
 	}
 
-	public void setMedicalRecordId(String medicalRecordId) {
-		this.medicalRecordId = medicalRecordId;
-	}
-
-	public String getFirtName() {
-		return firtName;
-	}
-
-	public void setFirtName(String firtName) {
-		this.firtName = firtName;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
 
 	public String getLastName() {
@@ -59,6 +84,22 @@ public class MedicalRecord {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public String getCity() {
@@ -77,14 +118,6 @@ public class MedicalRecord {
 		this.country = country;
 	}
 
-	public Gender getGender() {
-		return gender;
-	}
-
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
-
 	public LivingWithDiabetes getLivingWithDiabetes() {
 		return livingWithDiabetes;
 	}
@@ -92,5 +125,5 @@ public class MedicalRecord {
 	public void setLivingWithDiabetes(LivingWithDiabetes livingWithDiabetes) {
 		this.livingWithDiabetes = livingWithDiabetes;
 	}
-	
+
 }
--- src/test/java/com/savics/WebAppApplicationTests.java
+++ src/test/java/com/savics/WebAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ensat;
+package com.savics;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
